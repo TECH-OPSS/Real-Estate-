0728f5e7d7f35b0333abd160a6a2d24a08fcbd2e
Brandon <teambitsandy@gmail.com>
2024-12-30 09:25:37 +0300

    changes

---
+++ src/assets/assets.js
@@ -0,0 +1,25 @@
+import logo from './logo.jpeg'
+import real from './real.jpeg'
+import real2 from './real2.jpeg'
+
+export const assets ={
+    logo,
+    real,
+    real2,
+
+}
+
+export const projectData =[
+    {
+        title: "Skyline Haven",
+        price: "$2,500,000",
+        location:"California",
+        image: project_img_1
+    },
+    {
+        title: "Vista Verde",
+        price: "$300,000",
+        location:"San Francisco",
+        image: project_image_2
+    },
+]
